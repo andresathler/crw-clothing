2d7229793936a5ef94efd7ef51e7b7eb1200cd74
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2022-10-26 12:07:03 -0300

    reducer step 1 - useReducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from "react-redux";
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -13,15 +14,17 @@ const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        {/*<UserProvider>*/}
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        {/*</UserProvider>*/}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
