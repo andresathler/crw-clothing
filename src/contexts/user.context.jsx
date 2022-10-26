1ce69e8e190c599d7228009c9713dd8c124e70ff
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2022-10-26 08:54:42 -0300

    reducer step 1 - useReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -4,6 +4,7 @@ import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils';
+import {createAction} from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
@@ -33,7 +34,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) =>
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
